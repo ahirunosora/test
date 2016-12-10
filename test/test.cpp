f7c647cc7612907dfec9084a9258933f18669081
ahirunosora <hide261055@gmail.com>
2016-12-10 20:19:16 +0900

    完成(仮)

--- test/test.cpp
+++ test/test.cpp
@@ -6,46 +6,62 @@
 #include<stdlib.h>
 #include<string.h>
 
-#define n 11
+#define n 12
 
-//int compare(const void *arg1, const void *arg2)
-//{
+int compare(const void *arg1, const void *arg2)
+{
 	/* Compare all of both strings: */
-	//return strcmp(*(char**)arg1, *(char**)arg2);
-//}*/
+	const char* left_char = *(const char **)arg1;
+	const char* right_char = *(const char **)arg2;
+	return strcmp(left_char, right_char);
+}
 
-int *GetSA(const char *T, int *SA) {
+void GetSA(const char *T, int *SA) {
 	char *p;
 	char *str = (char *)malloc(2 * n);
-	char **suffix = (char **)malloc(n+1);
+	//char **suffix = (char **)malloc(n+1);
+	char *suffix[n];
 	int j = 0;
 	int k = 0;
 	for (int i = 0; i < n; i++)str[k++] = T[i];
 	for (int i = 0; i < n; i++)str[k++] = T[i];
 	p = str;
 	int length = k - 1;
-	//*suffix = p;
 	while (j < length) {
-		*(suffix + j) = p + j;
+		suffix[j] = p + j;
 		j++;
 		if (j == n)break;
 	}
 	char *T2 = str;
-	//qsort(*suffix,n,sizeof(char*),compare);
+	qsort(suffix,n,sizeof(*suffix),compare);
 	for (int i = 0; i < n; i++) SA[i] = suffix[i] - T2;
-	return SA;
 }
 
 int main() {
-	char T[] = { "mississippi" };
+	char T[] = { "mississippi$" };
 	int SA[n];
-	int *sa;
-	int b;
-	sa = GetSA(T,SA);
-	printf("T:%s\nSA:", T);
-	for (int i = 0; i<n; i++) {
-		printf("%d", sa[i]);
+	int i;
+	GetSA(T,SA);
+	char BWT[n];
+	for (i = 0; i < n; i++) {
+		if (SA[i] != 0) {
+			BWT[i] = T[SA[i] - 1];
+		}
+		else {
+			BWT[i] = '$';
+		}
 	}
+	
+	/*printf("T  :%s\nSA :", T);
+	for (i = 0; i<n; i++) {
+		printf("%2d", SA[i]);
+	}
+	printf("\nBWT:");
+	for (i = 0; i<n; i++) {
+		printf("%2c", BWT[i]);
+	}
+	printf("\n");*/
+
 	return 0;
 }
 
